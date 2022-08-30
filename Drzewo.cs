9833cd8a1794a0cdb9715adea21d7ee72e95caca
Francuz94 <Forward552@gmail.com>
2022-08-30 11:55:17 +0200

    Dodaj pliki projektów.

---
+++ Drzewo.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Algorytmy_i_struktury_danych_Lab
+{
+    class Drzewo
+    {
+
+        public static int iPre = 0;
+        public static int iIn = 0;
+        public static int iPos = 0;
+        public static int lisci = 0;
+        public Wezel korzen;
+        public Drzewo()
+        {
+            korzen = null;
+        }
+        public void add(int nowaWartość)
+        {
+            Wezel nowyWezel = new Wezel(nowaWartość);
+
+            if (korzen != null)
+            {
+                Wezel tmp = korzen;
+
+                while (true)
+                {
+                    if (nowaWartość <= tmp.wartosc)
+                    {
+                        if (tmp.lewy != null)
+                            tmp = tmp.lewy;
+
+                        else 
+                        {
+                            tmp.lewy = nowyWezel;
+                            lisci++;
+                            break;
+                        }
+                    }
+                    else if (nowaWartość > tmp.wartosc)
+                    {
+                        if (tmp.prawy != null)
+                            tmp = tmp.prawy;
+                        else
+                        {
+                            tmp.prawy = nowyWezel;
+                            lisci++;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                this.korzen = nowyWezel;
+                lisci++;
+            }
+        }
+        public void wypiszInOrder(Wezel korzen)
+        {
+            Wezel tmp;
+            tmp = korzen;
+            if (tmp == null) return;
+            wypiszInOrder(tmp.lewy);
+            if (iIn == 0) Console.Write($"\nDrzewo wypisane metodą In-Order:   {tmp.wartosc}");
+            else Console.Write(", " + tmp.wartosc);
+            iIn++;
+            wypiszInOrder(tmp.prawy);
+        }
+        public void wypiszPreOrder(Wezel korzen)
+        {
+            //Wezel tmp;
+            //tmp = korzen;
+            if (korzen == null) return;
+            if (iPre == 0) Console.Write($"\nDrzewo wypisane metodą Pre-Order:  {korzen.wartosc}");
+            else Console.Write(", " + korzen.wartosc);
+            iPre++;
+            wypiszPreOrder(korzen.lewy);
+            wypiszPreOrder(korzen.prawy);
+        }
+        public void wypiszPosOrder(Wezel korzen)
+        {
+            Wezel tmp;
+            tmp = korzen;
+            if (tmp == null) return;
+            wypiszPosOrder(tmp.lewy);
+            wypiszPosOrder(tmp.prawy);
+            if (iPos == 0) Console.Write($"\nDrzewo wypisane metodą Post-Order: {tmp.wartosc}");
+            else Console.Write(", " + tmp.wartosc);
+            
+            iPos++;
+        }
+        public void Sprawdz(Wezel korzen, int test)
+        {
+            Program.istniejeint = test;
+            if (korzen == null) return;
+            if (korzen.wartosc == test) Program.istnieje = true;
+            Sprawdz(korzen.lewy,test);
+            Sprawdz(korzen.prawy,test);
+        }
+    }
+    class Wezel
+    {
+        public int wartosc;
+        public Wezel lewy;
+        public Wezel prawy;
+        public Wezel(int wartosc) //konstruktor
+        {
+            this.wartosc = wartosc;
+            //Console.WriteLine($"Wartość w drzewie {Drzewo.lisci+1}");
+            lewy = null;
+            prawy = null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Algorytmy_i_struktury_danych_Lab
+{
+    class Program
+    {
+        public static int wartoscPobrana, istniejeint, tes;
+        public static bool istnieje = false;
+        static void Main(string[] args)
+        {
+            tes = 1;
+            Drzewo d = new Drzewo();
+            Console.WriteLine(" (Wprowadzająć wartość 890098 wprowadzi się wartości z przykładu z zajęć na moodle): ");
+            Console.Write("Wprowadz korzeń do drzewa: ");
+            wartoscPobrana = int.Parse(Console.ReadLine());
+            if (wartoscPobrana == 890098)
+            {
+                d.add(53);
+                d.add(28);
+                d.add(20);
+                d.add(19);
+                d.add(21);
+                d.add(34);
+                d.add(32);
+                d.add(38);
+                d.add(42);
+                d.add(76);
+                d.add(71);
+                d.add(73);
+                d.add(86);
+                tes = 0;
+            }
+            while (true)
+            {
+                if (wartoscPobrana == 0 || tes == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Zakończono wprowadzanie drzewa.\n");
+                    break;
+                }
+                else
+                {
+                    Console.Write("Wprowadz liść do drzewa: ");
+                    d.add(wartoscPobrana);
+                }
+                wartoscPobrana = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Wypisanie drzewa metodami:");
+            d.wypiszPreOrder(d.korzen);
+            d.wypiszInOrder(d.korzen);
+            d.wypiszPosOrder(d.korzen);
+            Console.WriteLine($"\nWprowadzono {Drzewo.lisci} wartości.\n");
+            while (true) { 
+            Console.Write("\npodaj liczbę jaką chcesz sprawdzić czy znajduje się w drzewie: ");
+                Sprawdz(d, int.Parse(Console.ReadLine()));
+            }
+            //Console.ReadKey();
+        }
+
+        private static void Sprawdz(Drzewo d,int test)
+        {
+            d.Sprawdz(d.korzen, test);
+            if (istnieje == true)
+            {
+                Console.Write($"\nWartość: {istniejeint}. Istnieje w drzewie\n");
+                istnieje = false;
+            }
+            else Console.Write($"\nWartość {istniejeint}. Nie istnieje w drzewie\n");
+        }
+    }  
+}
